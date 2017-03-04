ea7789d0b7000490b251362206b51bf2c9ceb4e0
lucasisasmendi <lucas.isasmendi@gmail.com>
2017-03-04 11:47:53 -0300

    user functions api

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,16 +6,75 @@ const db = require('../lib/mongodb-functions')
 APIuser.get('/test', (req, res) => {
   res.send('response from API user')
 })
-APIuser.post('/votedone', function (req, res) {
+APIuser.post('/addbyphone', function (req, res) {
   const phone = req.body.phone
   const hkid = req.body.hkid
-  db.insert('users', { _id: phone, hkid: hkid, voted: true }, function (err, cb) {
+  db.insert('users', { _id: phone, hkid: hkid, voted: false }, function (err, cb) {
+    if (err) {
+      if (err.code === 11000) {
+        res.send('phone already added')
+      } else {
+        res.send({err: 'error adding user by phone'})
+        console.log('error insert user', err)
+      }
+    } else {
+      res.send('user added by phone')
+    }
+  })
+})
+APIuser.get('/getbyphone', function (req, res) {
+  const phone = req.body.phone
+  db.findOne('users', { _id: phone }, function (err, record) {
+    if (err) {
+      res.send({err: 'error searching user by phone'})
+      console.log('error searching user - getbyphone', err)
+    } else if (record) {
+      res.send('user exists', record)
+    } else {
+      res.send('phone doesn`t exist')
+    }
+  })
+})
+APIuser.get('/getbytid', function (req, res) {
+  const telegramid = req.body.telegramid
+  db.findOne('users', { tid: telegramid }, function (err, record) {
+    if (err) {
+      res.send({err: 'error searching user by telegramid'})
+      console.log('error searching user - getbytelegramid', err)
+    } else if (record) {
+      record.signed_up = true
+      res.send('user exists', record)
+    } else {
+      res.send('telegramid doesn`t exist')
+    }
+  })
+})
+APIuser.post('/signup', function (req, res) {
+  const phone = req.body.phone
+  const hkid = req.body.hkid
+  const tid = req.body.tid
+  db.insert('users', { _id: phone, hkid: hkid, tid: tid, voted: false }, function (err, cb) {
+    if (err) {
+      if (err.code === 11000) {
+        res.send('phone already added')
+      } else {
+        res.send({err: 'error adding user by phone'})
+        console.log('error insert user', err)
+      }
+    } else {
+      res.send('user added by phone')
+    }
+  })
+})
+APIuser.post('/votedone', function (req, res) {
+  const phone = req.body.phone
+  db.update('users', { _id: phone }, {$set: {voted: true}}, function (err, cb) {
     if (err) {
       if (err.code === 11000) {
         res.send('phone already vote')
       } else {
-        res.send({err: 'error adding voters'})
-        console.log('error insert user', err)
+        res.send({err: 'error updating user'})
+        console.log('error updating user', err)
       }
     } else {
       res.send('vote done')
